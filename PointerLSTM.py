40eb1ceb35e66bcdbdc7a5e78d5f8ef3d52f9d39
shagun Sodhani <sshagunsodhani@gmail.com>
2017-08-17 15:04:55 +0530

    updated .gitignore and updated PointerLSTM and train.py for keras2.0

--- PointerLSTM.py
+++ PointerLSTM.py
@@ -1,9 +1,10 @@
-from keras import initializations
-from keras.layers.recurrent import time_distributed_dense
-from keras.activations import tanh, softmax
-from keras.layers import LSTM
-from keras.engine import InputSpec
+# from keras import initializations
 import keras.backend as K
+from keras.activations import tanh, softmax
+from keras.engine import InputSpec
+from keras.layers import LSTM
+from keras.layers.recurrent import Recurrent
+from keras.layers.recurrent import _time_distributed_dense
 
 
 class PointerLSTM(LSTM):
@@ -12,19 +13,31 @@ class PointerLSTM(LSTM):
         self.input_length = []
         super(PointerLSTM, self).__init__(*args, **kwargs)
 
+    def get_initial_states(self, x_input):
+        return Recurrent.get_initial_state(self, x_input)
+
     def build(self, input_shape):
         super(PointerLSTM, self).build(input_shape)
         self.input_spec = [InputSpec(shape=input_shape)]
-        init = initializations.get('orthogonal')
-        self.W1 = init((self.hidden_shape, 1))
-        self.W2 = init((self.hidden_shape, 1))
-        self.vt = init((input_shape[1], 1))
+        # init = initializations.get('orthogonal')
+        self.W1 = self.add_weight(name="W1",
+                                  shape=(self.hidden_shape, 1),
+                                  initializer="uniform",
+                                  trainable=True)
+        self.W2 = self.add_weight(name="W2",
+                                  shape=(self.hidden_shape, 1),
+                                  initializer="uniform",
+                                  trainable=True)
+        self.vt = self.add_weight(name="vt",
+                                  shape=(input_shape[1], 1),
+                                  initializer='uniform',
+                                  trainable=True)
         self.trainable_weights += [self.W1, self.W2, self.vt]
 
     def call(self, x, mask=None):
         input_shape = self.input_spec[0].shape
         en_seq = x
-        x_input = x[:, input_shape[1]-1, :]
+        x_input = x[:, input_shape[1] - 1, :]
         x_input = K.repeat(x_input, input_shape[1])
         initial_states = self.get_initial_states(x_input)
 
@@ -41,17 +54,17 @@ class PointerLSTM(LSTM):
         return outputs
 
     def step(self, x_input, states):
-    	#print "x_input:", x_input, x_input.shape
-    	# <TensorType(float32, matrix)>
-    	
+        # print "x_input:", x_input, x_input.shape
+        # <TensorType(float32, matrix)>
+
         input_shape = self.input_spec[0].shape
         en_seq = states[-1]
         _, [h, c] = super(PointerLSTM, self).step(x_input, states[:-1])
 
         # vt*tanh(W1*e+W2*d)
         dec_seq = K.repeat(h, input_shape[1])
-        Eij = time_distributed_dense(en_seq, self.W1, output_dim=1)
-        Dij = time_distributed_dense(dec_seq, self.W2, output_dim=1)
+        Eij = _time_distributed_dense(en_seq, self.W1, output_dim=1)
+        Dij = _time_distributed_dense(dec_seq, self.W2, output_dim=1)
         U = self.vt * tanh(Eij + Dij)
         U = K.squeeze(U, 2)
 
@@ -61,4 +74,7 @@ class PointerLSTM(LSTM):
 
     def get_output_shape_for(self, input_shape):
         # output shape is not affected by the attention component
+        return (input_shape[0], input_shape[1], input_shape[1])
+
+    def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[1], input_shape[1])
